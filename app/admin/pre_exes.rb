f6c7c8de76147531dbfeadf0b11b2a70910a8bf8
Raghu Betina <raghu@firstdraft.com>
2018-10-25 11:37:06 +0000

    Whitelist columns in PreEx for ActiveAdmin

--- app/admin/pre_exes.rb
+++ app/admin/pre_exes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register PreEx do
+
+ permit_params :contenido_id, :molde_id, :enunciado, :imagen, :resp_correcta, :w1, :w2, :w3, :w4, :op1, :op2, :op3, :op4, :op5, :hint, :dificultad, :habilidad
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
