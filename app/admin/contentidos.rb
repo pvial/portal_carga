84592e08c74e87d3ce7435da682f122f83f65604
Raghu Betina <raghu@firstdraft.com>
2018-10-25 11:37:13 +0000

    Whitelist columns in Contentido for ActiveAdmin

--- app/admin/contentidos.rb
+++ app/admin/contentidos.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Contentido do
+
+ permit_params :eje_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
