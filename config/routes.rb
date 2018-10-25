ea6dc43cdefa362a4c5ecfb83813bfdb54ee3ad9
Raghu Betina <raghu@firstdraft.com>
2018-10-25 11:37:01 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "pre_exes#index"
   devise_for :aprobadors
   # Routes for the Aprobador resource:
