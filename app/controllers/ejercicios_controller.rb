96d4ab644f23a898836ff4903039abc06a709a29
Raghu Betina <raghu@firstdraft.com>
2018-10-25 11:38:39 +0000

    Only ejercicio user can modify ejercicio

--- app/controllers/ejercicios_controller.rb
+++ app/controllers/ejercicios_controller.rb
@@ -1,4 +1,14 @@
 class EjerciciosController < ApplicationController
+  before_action :current_creador_must_be_ejercicio_creador, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_creador_must_be_ejercicio_creador
+    ejercicio = Ejercicio.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_creador == ejercicio.creador
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @ejercicios = Ejercicio.all
 
