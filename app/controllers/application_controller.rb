ee5794a25cc6d672b57e0321c25c4e16fad9a177
Raghu Betina <raghu@firstdraft.com>
2018-10-25 11:39:24 +0000

    Comment protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_creador!
 
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 end
